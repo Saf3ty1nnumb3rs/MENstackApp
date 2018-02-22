7b5ad38b3293f5e75c9ae6eb8dcf85e34b14fef9
Josh Sample <joshuawsample@gmail.com>
2018-02-21 22:40:12 -0500

    Completed countrySchema

--- db/schemas/countrySchema.js
+++ db/schemas/countrySchema.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
+const whiskySchema = require('./whiskySchema')
 const Schema = mongoose.Schema
 
+const countrySchema = new Schema({
+    country: String,
+    primaryWhiskyStyle: String,
+    topSellingWhisky: String,
+    whiskyProduced: [whiskySchema]
+})
 
 
 
-
-module.exports =
+module.exports = countrySchema
