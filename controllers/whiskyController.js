158713026c6736cdb22e0a20251c37a05a1893d8
Josh Sample <joshuawsample@gmail.com>
2018-02-22 17:23:55 -0500

    Button routing corrections to and from nested objects

--- controllers/whiskyController.js
+++ controllers/whiskyController.js
@@ -29,5 +29,39 @@ router.get('/:id', (req, res) => {
 
 })
 
-//WHISKY
+//WHISKY EDIT------------------------------GET//
+
+router.get('/:id/edit', (req, res) => {
+
+    
+    Country.findById(req.params.companyId).then((country) => {
+        const whisky = country.whisky.id(req.params.id)
+        res.render('whisky/edit', {
+            countryId: req.params.countryId,
+            whisky: whisky
+        })
+    })
+  })
+    
+  
+  // UPDATE
+  // PUT/PATCH
+  router.patch('/:id', (req, res) => {
+    Company.findById(req.params.companyId).then((company) => {
+  
+      // We don't have a nice method like findByIdAndUpdate here
+      // so instead we need to manually change the sodas values
+      const soda = company.sodas.id(req.params.id)
+      soda.name = req.body.name
+      soda.price = req.body.price
+      soda.packaging = req.body.packaging
+      soda.quantitySold = req.body.quantitySold
+  
+      // Then Save the company
+      return company.save()
+    }).then((updatedCompany) => {
+      res.redirect(`/companies/${updatedCompany._id}/sodas/${req.params.id}`)
+    })
+  })
+  
 module.exports = router
