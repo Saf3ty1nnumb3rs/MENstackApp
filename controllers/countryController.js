aba2cd81f7f234744dd14dc9250997cc04de4f9f
Josh Sample <joshuawsample@gmail.com>
2018-02-22 10:09:37 -0500

    Country index route up and populated

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -1,4 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const Country = require('../models/country')
 
+
+
+
+
+//COUNTRY INDEX------------------------GET//
+router.get('/', function(req, res, next) {
+    
+    Country.find().then((countries) => {
+        res.render('country/index', { 
+        countries:countries  })
+  })
+  
+});
+  
+  
 module.exports = router
