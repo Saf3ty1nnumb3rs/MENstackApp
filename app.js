b67a12c84b43dcfa4f95e5ff8f284a9871303bdd
Josh Sample <joshuawsample@gmail.com>
2018-02-22 11:26:05 -0500

    Country views and country show both operational

--- app.js
+++ app.js
@@ -41,19 +41,19 @@ app.use(methodOverride('_method'))
 
 app.use('/', index)
 app.use('/users', usersController)
-app.use('/whisky', whiskyController)
+app.use('/country/:countryId/whisky', whiskyController)
 app.use('/country', countryController)
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found')
   err.status = 404
   next(err)
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
--- controllers/countryController.js
+++ controllers/countryController.js
@@ -7,14 +7,26 @@ const Country = require('../models/country')
 
 
 //COUNTRY INDEX------------------------GET//
-router.get('/', function(req, res, next) {
-    
+router.get('/', (req, res) => {
+
     Country.find().then((countries) => {
-        res.render('country/index', { 
-        countries:countries  })
-  })
-  
-});
-  
-  
+        res.render('country/index', {
+            countries: countries
+        })
+    })
+
+})
+
+//COUNTRY SHOW-------------------------GET//
+router.get('/:id', (req, res) => {
+
+    Country.findById(req.params.id).then((country) => {
+        res.render('country/show', {
+            country: country
+        })
+    })
+
+})
+
+
 module.exports = router
