b485158abf34e31410899d19de0f0ef5e06c5ba4
Josh Sample <joshuawsample@gmail.com>
2018-02-26 10:43:07 -0500

    More style and html adjustments

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,33 +1,36 @@
 const ClickController = {
     // Need to set if sttement to check for has class 'is-active' then do below if false
-    
-    
-    
-        hamburgerHelper: function(){
-    
-            if($('button.hamburger').hasClass('is-active')){
-                
-                $('.menu-overlay').removeClass('open');
-                $('.contact-build').removeClass('closed');
-                $('button.hamburger').removeClass('is-active');
-                
-    // Set else statement to perform the opposite of above - easy-peezy
-                } else {
-                    $('button.hamburger').addClass('is-active');
-                    $('.menu-overlay').addClass('open');
-                    $('.contact-build').addClass('closed');
-    
-                }
-    
+
+
+
+    hamburgerHelper: () => {
+
+        if ($('button.hamburger').hasClass('is-active')) {
+
+            $('.menu-overlay').removeClass('open');
+            $('button.hamburger').removeClass('is-active');
+
+            // Set else statement to perform the opposite of above - easy-peezy
+        } else {
+            $('button.hamburger').addClass('is-active');
+            $('.menu-overlay').addClass('open');
+
+        }
+
     },
-    
-    
-    
-    
+
+    aboutHelper: () => {
+        $('.menu-overlay').removeClass('open');
+        $('button.hamburger').removeClass('is-active');
     }
 
 
 
-    window.onload = function () {
-        $('.hamburger').on('click', ClickController.hamburgerHelper)
-    }
+}
+
+
+
+window.onload = function () {
+    $('.hamburger').on('click', ClickController.hamburgerHelper)
+    $('#about-click').on('click', ClickController.aboutHelper)
+}
